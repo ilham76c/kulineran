2514800c3e125f2a51b44b04ffd7d32d5b4fc3d6
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-09-29 23:50:52 +0700

    add card product layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,20 @@
     
     <div class="container">
       <Hero/>
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Best <strong>Food</strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right"><b-icon-eye></b-icon-eye> Lihat Semua</router-link>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,12 +27,37 @@
 
 import Navbar from '../components/Navbar.vue'
 import Hero from '../components/Hero.vue'
+import CardProduct from '../components/CardProduct.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     Navbar,
-    Hero
+    Hero,
+    CardProduct
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/best-products')
+      .then((response) => {
+        // handle success
+        this.setProducts(response.data);
+        console.log('Berhasil : ', response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log('Gagal : ', error);
+      });
   }
 }
 </script>
