ab8c364b560304bd08ce2384825b1531973ebed1
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-13 00:24:52 +0700

    mengubah navbar dari menggunakan "bootstrap" menjadi "bootstrap-vue" agar tampilan bisa mobile responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,35 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light">
+<div>
+  <b-navbar toggleable="lg" type="light">
+    <div class="container">
+      <b-navbar-brand href="#">Kulineran</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/foods">Foods</router-link>
+          </li>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+           <li class="nav-item">
+            <router-link class="nav-link" to="/keranjang">
+                Keranjang <b-icon-bag></b-icon-bag>
+                <span class="badge badge-success ml-2">{{ updateKeranjang ? updateKeranjang.length : jumlah_pesanan.length }}</span>
+            </router-link>
+          </li>
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
+  </b-navbar>
+</div>
+  <!-- <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container">
     <a class="navbar-brand" href="#">Kulineran</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,7 +55,7 @@
       </ul>
     </div>
   </div>
-</nav>
+  </nav> -->
 </template>
 
 <script>
